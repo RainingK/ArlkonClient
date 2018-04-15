474b30fad43d930ece8b6063072d1d1376630778
shahlin <shahlin44@gmail.com>
2018-04-15 08:58:44 +0400

    Server connected

--- src/controllers/IndexController.java
+++ src/controllers/IndexController.java
@@ -5,9 +5,14 @@
  */
 package controllers;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -15,13 +20,32 @@ import javafx.fxml.Initializable;
  * @author DELL
  */
 public class IndexController implements Initializable {
+    
+    @FXML JFXTextField username_input;
+    @FXML JFXButton login_btn;
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+        login_btn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+ 
+        @Override
+        public void handle(MouseEvent t) {
+            login();
+        }
+    });
+    }
+
+    private void login(){
+        String username = username_input.getText();
+        String output = hello(username);
+        System.out.println(output);
+    }
+
+    private static String hello(java.lang.String name) {
+        webservices.SessionWS_Service service = new webservices.SessionWS_Service();
+        webservices.SessionWS port = service.getSessionWSPort();
+        return port.hello(name);
+    }
     
 }
