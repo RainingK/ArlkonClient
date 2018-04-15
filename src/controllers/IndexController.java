d6a42b01366295f1760bea27034945ddacf4e86b
shahlin <shahlin44@gmail.com>
2018-04-16 00:13:44 +0400

    Added signup form with transition

--- src/controllers/IndexController.java
+++ src/controllers/IndexController.java
@@ -6,13 +6,19 @@
 package controllers;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.TranslateTransition;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.transform.Translate;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -21,31 +27,73 @@ import javafx.scene.input.MouseEvent;
  */
 public class IndexController implements Initializable {
     
-    @FXML JFXTextField username_input;
+    @FXML JFXTextField login_username_input;
+    @FXML JFXPasswordField login_password_input;
     @FXML JFXButton login_btn;
-
+    
+    @FXML Pane right_signup_pane;
+    @FXML Label noAccountLabel;
+    @FXML Label haveAccountLabel;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        final TranslateTransition transition = new TranslateTransition(Duration.seconds(0.5), right_signup_pane);
+
+        // Show Sign up form when 'Don't have an account?' label is clicked
+        noAccountLabel.setOnMouseClicked(new EventHandler<MouseEvent>(){            
+            @Override
+            public void handle(MouseEvent t){
+                transition.setDelay(Duration.seconds(0.2));
+                transition.setToX(-275);
+                transition.setCycleCount(1);
+                transition.play();
+            }
+        });
+        
+        // Hide Sign up form when 'Already have an account?' label is clicked
+        haveAccountLabel.setOnMouseClicked(new EventHandler<MouseEvent>(){            
+            @Override
+            public void handle(MouseEvent t){
+                transition.setDelay(Duration.seconds(0.2));
+                transition.setToX(0);
+                transition.setCycleCount(1);
+                transition.play();
+            }
+        });
+        
         login_btn.setOnMouseClicked(new EventHandler<MouseEvent>() {
- 
-        @Override
-        public void handle(MouseEvent t) {
-            login();
-        }
-    });
+            @Override
+            public void handle(MouseEvent t) {
+                // Disable button when clicked once
+                login_btn.setDisable(true);
+                
+                // Get user inputs
+                String username = login_username_input.getText();
+                String password = login_password_input.getText();
+                
+                Thread login_thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Boolean login = login(username, password);
+                        
+                        if(login){
+                            System.out.println("Logged in!");
+                        } else {
+                            login_btn.setDisable(false);
+                        }
+                    }
+                });
+                
+                login_thread.start();
+            }
+        });
     }
-
-    private void login(){
-        String username = username_input.getText();
-        String output = hello(username);
-        System.out.println(output);
-    }
-
-    private static String hello(java.lang.String name) {
+    
+    private static Boolean login(java.lang.String username, java.lang.String password) {
         webservices.SessionWS_Service service = new webservices.SessionWS_Service();
         webservices.SessionWS port = service.getSessionWSPort();
-        return port.hello(name);
+        return port.login(username, password);
     }
     
 }
