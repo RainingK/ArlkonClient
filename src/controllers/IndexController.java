063781f48a97b7b80942d19641099195ffe5afab
Ammar <ammar_98@live.com>
2018-04-17 18:18:02 +0400

    Added Sign Up feature

--- src/controllers/IndexController.java
+++ src/controllers/IndexController.java
@@ -15,6 +15,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -23,6 +24,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -40,18 +44,27 @@ public class IndexController implements Initializable {
     @FXML JFXButton login_btn;
     
     @FXML Pane right_signup_pane;
+    @FXML TextField signup_username_input;
+    @FXML TextField signup_email_input;
+    @FXML JFXPasswordField signup_password_input;
+    @FXML JFXPasswordField signup_confirm_password_input;
+    @FXML JFXButton signup_btn;
+    
     @FXML Label noAccountLabel;
     @FXML Label haveAccountLabel;
     
     @FXML Pane main_window;
     
-    //@FXML ImageView close_btn;
-    //@FXML ImageView minimize_btn;
+    @FXML ImageView close_btn;
+    @FXML ImageView minimize_btn;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
         final TranslateTransition transition = new TranslateTransition(Duration.seconds(0.5), right_signup_pane);
+        signup_btn.setDisable(true);
+        close_btn.setPickOnBounds(true);
+        minimize_btn.setPickOnBounds(true);
 
         // Show Sign up form when 'Don't have an account?' label is clicked
         noAccountLabel.setOnMouseClicked(new EventHandler<MouseEvent>(){            
@@ -92,7 +105,7 @@ public class IndexController implements Initializable {
                         
                         if(login){
                             // Send user to home page
-                            fadeOutTransition();
+                            fadeOutTransition("/views/binaryTrading.fxml");
                         } else {
                             // Change text field border bottom color to red to indicate error
                             login_username_input.setStyle("-jfx-focus-color: #be222c; -jfx-unfocus-color: #be222c;");
@@ -105,9 +118,39 @@ public class IndexController implements Initializable {
                 login_thread.start();
             }
         });
+        
+        signup_btn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                
+                // Get user inputs
+                String username = signup_username_input.getText().trim();
+                String email = signup_email_input.getText().trim();
+                String password = signup_password_input.getText().trim();
+                String Cpassword = signup_confirm_password_input.getText().trim();
+                
+                Thread SignUp_Thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Boolean signUp = signUp(username, email, password, Cpassword);
+                        
+                        if(signUp){
+                            fadeOutTransition("/views/home.fxml");
+                        } else {
+                            System.out.println("Error");
+                        }
+                    }
+                });
+                
+                SignUp_Thread.start();
+            }
+        });
+        
+        closeProgram(close_btn);
+        minimizeProgram(minimize_btn);
     }
     
-    private void fadeOutTransition(){
+    private void fadeOutTransition(String sceneLoc){
         FadeTransition fadeTransition = new FadeTransition();
         fadeTransition.setDuration(Duration.millis(300));
         fadeTransition.setNode(main_window);
@@ -116,7 +159,7 @@ public class IndexController implements Initializable {
         
         fadeTransition.setOnFinished((ActionEvent event) -> {
             try {
-                loadNextScene();
+                loadNextScene(sceneLoc);
             } catch (IOException ex) {
                 Logger.getLogger(IndexController.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -125,9 +168,9 @@ public class IndexController implements Initializable {
         fadeTransition.play();
     }
     
-    private void loadNextScene() throws IOException {
+    private void loadNextScene(String sceneLoc) throws IOException {
         Parent nextView;
-        nextView = (Pane) FXMLLoader.load(getClass().getResource("/views/binaryTrading.fxml"));
+        nextView = (Pane) FXMLLoader.load(getClass().getResource(sceneLoc));
         
         Scene nextScene = new Scene(nextView);
         Stage curStage = (Stage) main_window.getScene().getWindow();
@@ -135,10 +178,39 @@ public class IndexController implements Initializable {
         curStage.setScene(nextScene);
     }
     
+    public void closeProgram(ImageView img){
+            img.setOnMouseClicked((MouseEvent e) -> {
+            Platform.exit();
+        });
+    }
+    
+    @FXML
+    void allowSignup(KeyEvent event) {        
+        if((signup_username_input.getText().trim().length() < 4) || (signup_email_input.getText().trim().length() < 7) ||
+                (signup_password_input.getText().trim().length() < 6) || (signup_confirm_password_input.getText().trim().length() < 6)){
+            signup_btn.setDisable(true);
+        } else {
+            signup_btn.setDisable(false);
+        }
+    }
+    
+    public void minimizeProgram(ImageView img){
+        img.setOnMouseClicked((MouseEvent e) -> {
+            Stage stage = null;
+            stage = (Stage) ((ImageView) e.getSource()).getScene().getWindow();
+            stage.setIconified(true);
+        });
+    }
+    
     private static Boolean login(java.lang.String username, java.lang.String password) {
         webservices.SessionWS_Service service = new webservices.SessionWS_Service();
         webservices.SessionWS port = service.getSessionWSPort();
         return port.login(username, password);
     }
-    
+
+    private static Boolean signUp(java.lang.String username, java.lang.String email, java.lang.String password, java.lang.String cpassword) {
+        webservices.SessionWS_Service service = new webservices.SessionWS_Service();
+        webservices.SessionWS port = service.getSessionWSPort();
+        return port.signUp(username, email, password, cpassword);
+    }    
 }
