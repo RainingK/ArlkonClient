1f7bd6af0472ba344078bcb1f015ac13372eee34
shahlin <shahlin44@gmail.com>
2018-05-05 01:40:43 +0400

    Logs out even if user closes the application using taskbar

--- src/main/ArlkonClient.java
+++ src/main/ArlkonClient.java
@@ -5,13 +5,24 @@
  */
 package main;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
+import java.io.File;
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
+import utils.Transition;
 
 /**
  *
@@ -28,6 +39,23 @@ public class ArlkonClient extends Application {
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setScene(scene);
         primaryStage.show();
+        
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                logout();
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+    }
+    
+    private void logout(){
+        File file = new File("user_data.txt");
+        
+        if(file.exists() && !file.isDirectory()){
+            file.delete();
+        }
     }
 
     /**
